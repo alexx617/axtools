9809776b8721aed9caa23e48dc266b78c49d5030
alexx617 <290477385@qq.com>
2018-08-01 13:39:47 +0800

    update:修改相差时间fn

--- utils/time.js
+++ utils/time.js
@@ -254,13 +254,24 @@ module.exports = {
     },
 
     //获取两个时间段差多久
-    checkTime(t1){
-        let timestamp1 = new Date(Number(t1)),
-            timestamp2 = new Date();
-        let date = timestamp1.getTime() - timestamp2.getTime();
-        return date > 0 ? false :true;
-        // 如果相减大于0，则1比2时间大（晚）//已结束
-        // 如果相减小于0，则1比2时间小（早）//未结束
-    }, 
+    timeFn(time,fmt = 1) {//di作为一个变量传进来
+        //如果时间格式是正确的，那下面这一步转化时间格式就可以不用了
+        var dateBegin = new Date(time.replace(/-/g, "/"));//将-转化为/，使用new Date
+        var dateEnd = new Date();//获取当前时间
+        var dateDiff = dateEnd.getTime() - dateBegin.getTime();//时间差的毫秒数
+        var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
+        var leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
+        var hours=Math.floor(leave1/(3600*1000))//计算出小时数
+        //计算相差分钟数
+        var leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
+        var minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+        //计算相差秒数
+        var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+        var seconds=Math.round(leave3/1000)
+        if(fmt){
+            return `相差${dayDiff}天${hours}小时${minutes}分钟${seconds}秒`
+        }
+        return `${dateDiff}时间差的毫秒数,${dayDiff}计算出相差天数,${leave1}计算天数后剩余的毫秒数,${hours}计算出小时数,${minutes}计算相差分钟数,${seconds}计算相差秒数`
+    }
 
 }
