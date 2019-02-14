8ed4b42889f4326da0f046baed31b8d64f3d215d
hejiaxin <hejiaxin@weidian.com>
2019-02-14 10:22:20 +0800

    add:增加节流防抖

--- utils/other.js
+++ utils/other.js
@@ -112,6 +112,68 @@ export default {
         return !![].map && document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0;
     },
 
+
+
+    /**
+     * https://blog.csdn.net/ligang2585116/article/details/75003436
+     * debounce： 去抖，保证多少ms内不再被触发时就会执行一次,把触发非常频繁的事件合并成一次执行,对于键盘事件，当用户输入比较频繁的时候，可以通过debounce合并键盘事件处理,对于ajax请求的情况，例如当页面下拉超过一定返回就通过ajax请求新的页面内容，这时候可以通过debounce合并ajax请求事件
+     * 
+     * throttle：节流阀，保证多少ms内只执行一次。设置一个阀值，在阀值内，把触发的事件合并成一次执行；当到达阀值，必定执行一次事件,对于键盘事件，当用户输入非常频繁，但是我们又必须要在一定时间内（阀值）内执行处理函数的时候，就可以使用throttle
+     * 
+     *  // 执行1次(最后一次点击1000ms后)
+        btnDom.addEventListener('click', debounce(clickBtn, 1000));
+        // 执行3次(点击立即执行一次、1000ms后执行一次，2000ms后执行一次)
+        btnDom.addEventListener('click', throttle(clickBtn, 1000));
+     * 
+     * 文本输入、自动完成，keyup 事件, 搜索框输入发送ajax请求，监听onchange or keyup 事件进行查询,需要debounce
+     * 鼠标移动，mousemove 事件，需要throttle
+     * DOM 元素动态定位，window 对象的resize 和 scroll 事件，需要throttle
+     */
+    /**
+     * 
+     * @param {*} action 延迟毫秒后执行的函数
+     * @param {*} delay 0 或者更大的毫秒数。 对于事件回调，大约100或250毫秒（或更高）的延迟是最有用的
+     * 用法:
+     *  function resizeHandler() {
+            console.log("resize");
+        }
+        window.onresize = debounce(resizeHandler, 300);
+    */
+    debounce(action, delay) {
+        let timer = null;
+        return function () {
+            let self = this,
+                args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                action.apply(self, args)
+            }, delay);
+        }
+    },
+
+    /**
+     *
+     * @param {*} action 延迟毫秒后执行的函数
+     * @param {*} delay 0 或者更大的毫秒数。 对于事件回调，大约100或250毫秒（或更高）的延迟是最有用的
+     * 用法:
+     *  function resizeHandler() {
+            console.log("resize");
+        }
+        window.onresize = throttle(resizeHandler, 300);
+    */
+    throttle(action, delay) {
+        let statTime = 0;
+        return function () {
+            let currTime = +new Date();
+            if (currTime - statTime > delay) {
+                action.apply(this, arguments);
+                statTime = currTime;
+            }
+        }
+    },
+
+
+
     /**
      * @desc   函数节流。
      * 适用于限制`resize`和`scroll`等函数的调用频率
@@ -126,7 +188,7 @@ export default {
      *                                    如果debounceMode是false，`callback`在`delay` ms之后执行。
      * @return {Function}  新的节流函数
      */
-    throttle(delay, noTrailing, callback, debounceMode) {
+    throttleV2(delay, noTrailing, callback, debounceMode) {
         var timeoutID;
         var lastExec = 0;
         if (typeof noTrailing !== 'boolean') {
@@ -175,8 +237,89 @@ export default {
     *
     * @return {Function} 新的防抖函数。
     */
-    debounce(delay, atBegin, callback) {
+    debounceV2(delay, atBegin, callback) {
         return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false);
+    },
+
+    /**
+     * 创建并返回一个像节流阀一样的函数，当重复调用函数的时候，最多每隔 wait毫秒调用一次该函数
+     * @param func 执行函数
+     * @param wait 时间间隔
+     * @param options 如果你想禁用第一次首先执行的话，传递{leading: false}，
+     *                如果你想禁用最后一次执行的话，传递{trailing: false}
+     * @returns {Function}
+     */
+    throttleV3(func, wait, options) {
+        var context, args, result;
+        var timeout = null;
+        var previous = 0;
+        if (!options) options = {};
+        var later = function () {
+            previous = options.leading === false ? 0 : new Date().getTime();
+            timeout = null;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        };
+        return function () {
+            var now = new Date().getTime();
+            if (!previous && options.leading === false) previous = now;
+            var remaining = wait - (now - previous);
+            context = this;
+            args = arguments;
+            if (remaining <= 0 || remaining > wait) {
+                if (timeout) {
+                    clearTimeout(timeout);
+                    timeout = null;
+                }
+                previous = now;
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            } else if (!timeout && options.trailing !== false) {
+                timeout = setTimeout(later, remaining);
+            }
+            return result;
+        };
+    },
+
+    /**
+     * 防反跳。func函数在最后一次调用时刻的wait毫秒之后执行！
+     * @param func 执行函数
+     * @param wait 时间间隔
+     * @param immediate 为true，debounce会在wai 时间间隔的开始调用这个函数
+     * @returns {Function}
+     */
+    debounceV3(func, wait, immediate) {
+        var timeout, args, context, timestamp, result;
+
+        var later = function () {
+            var last = new Date().getTime() - timestamp; // timestamp会实时更新
+
+            if (last < wait && last >= 0) {
+                timeout = setTimeout(later, wait - last);
+            } else {
+                timeout = null;
+                if (!immediate) {
+                    result = func.apply(context, args);
+                    if (!timeout) context = args = null;
+                }
+            }
+        };
+
+        return function () {
+            context = this;
+            args = arguments;
+            timestamp = new Date().getTime();
+            var callNow = immediate && !timeout;
+
+            if (!timeout) {
+                timeout = setTimeout(later, wait);
+            }
+            if (callNow) {
+                result = func.apply(context, args);
+                context = args = null;
+            }
+            return result;
+        };
     },
 
 
@@ -184,7 +327,7 @@ export default {
      * @desc   生成下载
      * @param  {href,title} 
      */
-    download (href, title ){
+    download(href, title) {
         //火狐专用
         if (navigator.userAgent.indexOf("Firefox") > 0) {
             window.location = href
