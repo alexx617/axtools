2ee65abc0d0f646ecb5a6c407b90eb6514d4b010
hejiaxin <hejiaxin@weidian.com>
2020-03-12 10:28:10 +0800

    add

--- utils/time.js
+++ utils/time.js
@@ -195,6 +195,11 @@ export default {
             h = Math.floor(t / 1000 / 60 / 60 % 24);//h = Math.floor(t / 1000 / 60 / 60);不显示天数的话使用这个
             m = Math.floor(t / 1000 / 60 % 60);
             s = Math.floor(t / 1000 % 60);
+            // 补零
+            d = d < 10 ? ("0" + d) : d;
+            h = h < 10 ? ("0" + h) : h;
+            m = m < 10 ? ("0" + m) : m;
+            s = s < 10 ? ("0" + s) : s;
         }
         return { d, h, m, s };
     },
