97d590cc8e38bd748af8ab5b1e3828fe982da8b9
hejiaxin <hejiaxin@weidian.com>
2020-07-31 15:51:01 +0800

    add

--- utils/money.js
+++ utils/money.js
@@ -372,5 +372,33 @@ export default {
         } else {
             return num;
         }
+    },
+    // 分转元保留2位小数(金额)
+    regFenToYuan(fen) {
+        var num = fen;
+        num = fen * 0.01;
+        num += '';
+        var reg = num.indexOf('.') > -1 ? /(\d{1,3})(?=(?:\d{3})+\.)/g : /(\d{1,3})(?=(?:\d{3})+$)/g;
+        num = num.replace(reg, '$1');
+        num = this.toDecimal2(num);
+        return num
+    },
+    // 强制保留2位小数
+    toDecimal2(x) {
+        var f = parseFloat(x);
+        if (isNaN(f)) {
+            return false;
+        }
+        var f = Math.round(x * 100) / 100;
+        var s = f.toString();
+        var rs = s.indexOf('.');
+        if (rs < 0) {
+            rs = s.length;
+            s += '.';
+        }
+        while (s.length <= rs + 2) {
+            s += '0';
+        }
+        return s;
     }
 }
