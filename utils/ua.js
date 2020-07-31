87614db3f89da8b8a94199ebb855def92b04351a
hejiaxin <hejiaxin@weidian.com>
2020-07-31 16:08:12 +0800

    add

--- utils/ua.js
+++ utils/ua.js
@@ -190,6 +190,38 @@ export default {
         return eval('0' + comparator + cmp);
     },
 
+    /**
+     *  判断版本号,然后比较两个版本号相差多少个版本
+     *  compareVersions('1.2.0', '==', '1.2'); // true
+        compareVersions('00001', '==', '1.0.0'); // true
+        compareVersions('1.2.0', '<=', '1.2'); // true
+        compareVersions('2.2.0', '<=', '1.2'); // false
+    */
+    compareVersionsPart(v1, comparator, v2) {
+        if (!v1) { return true; }
+        var comparator = comparator == '=' ? '==' : comparator;
+        if (!['==', '===', '<', '<=', '>', '>=', '!=', '!=='].includes(comparator)) {
+            throw new Error('Invalid comparator. ' + comparator);
+        }
+        var v1parts = v1.split('.'), v2parts = v2.split('.');
+        var maxLen = Math.max(v1parts.length, v2parts.length);
+        var part1, part2;
+        var cmp = 0;
+        for (var i = 0; i < maxLen && !cmp; i++) {
+            part1 = parseInt(v1parts[i], 10) || 0;
+            part2 = parseInt(v2parts[i], 10) || 0;
+            if (part1 < part2)
+                cmp = 1;
+            if (part1 > part2)
+                cmp = -1;
+        }
+        let part = 0;
+        if (cmp) {
+            part = part1 - part2;
+        }
+        return part;
+    },
+
     /* example
     * 比较版本号
     * cmpVersion('1.1.8', '1.0.4'); // -> 1
